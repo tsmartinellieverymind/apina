e5a27321de6bffd068b6b84ea4606ad3bf9f99fe
Thiago Martinelli <tsmartinellieverymind>
2025-04-06 21:31:48 -0300

    New Arch

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const webhook = require('./routes/webhook');
+const webhook = require('./routes/webhook'); // arquivo de rota
 
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/webhook', webhook);
+// 👇 URL que o Twilio já está usando
+app.use('/whatsapp-webhook', webhook);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
